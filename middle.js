588f4252653d992d4829c5a16e92187bbe72cacc
Ian Sebleius <sebeliusancira@gmail.com>
2023-05-21 16:19:39 -0700

    wrote psudo code for middle function

---
+++ middle.js
@@ -0,0 +1,11 @@
+// Psudo code for middle.js
+
+// crear variable middle []
+// checar si arr.length es par o non
+// si es non
+  // middleIndex = Math.floor(arr.length / 2)
+  // middle.push(arr[middleIndex])
+// si es par
+  // middleIndexStart = (arr.length / 2) - 1
+  // middle.push(arr.slice(middleIndexStart, middleIndexStart + 2))
+// return middle
