bf60ce65517d198f387820f107db4cb5f4431acd
Ian Sebleius <sebeliusancira@gmail.com>
2023-05-27 13:03:28 -0700

    implemented function that uses a callback to scan through an object.

---
+++ findKey.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  const passed = `🍈🍈🍈 Assertion Passed: ${actual} === ${expected}`;
+  const failed = `🎯🎯🎯 Assertion Failed: ${actual} !== ${expected}`;
+  actual === expected ? console.log(passed) : console.log(failed);
+};
+
+const findKey = (object, cb) => {
+  for ( const e in object) {
+    if (cb(object[e])) {
+      console.log(e);
+      return e;
+    } else {
+      undefined;
+    }
+  }
+};
+
+const object = {
+  'Blue Hill': {stars: 1},
+  'Akaleri': {stars: 3},
+  'noma': {stars: 2},
+  'elBulli': {stars: 3},
+  'Ora': {stars: 2},
+  'Akelarre': {stars: 3},
+};
+
+assertEqual(findKey(object, (x) => x.stars === 2), 'noma');
+
