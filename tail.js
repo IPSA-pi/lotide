d31f2c090549cc19823395c0cf293165d34d004b
Ian Sebleius <sebeliusancira@gmail.com>
2023-06-07 20:56:41 -0700

    refactored tail function and added test to test folder

--- tail.js
+++ tail.js
@@ -1,18 +1,9 @@
-const assertEqual = function(actual, expected) {
-  const passed = `🍈🍈🍈 Assertion Passed: ${actual} === ${expected}`;
-  const failed = `🎯🎯🎯 Assertion Failed: ${actual} !== ${expected}`;
-  actual === expected ? console.log(passed) : console.log(failed);
-};
-
-let result;
-
 const tail = function(arr) {
+  let result;
   arr.length === 1 || arr.length === 0 ?
     result = [] :
       result = arr.slice(1);
   return result;
 };
 
-// eslint-disable-next-line max-len
-assertEqual(tail(['Hello', 'Lighthouse', 'Labs']).join(''), ['Lighthouse', 'Labs'].join(''));
-assertEqual(tail([]).join(), [].join());
+module.exports = tail;
